6252d21edf7dd97cb77d5c1d96694a7f5b921fce
Vonsec_GE <147484976+Vonsec@users.noreply.github.com>
2024-10-28 19:19:22 +0300

    Update Task_2.py
    Replaced round() with math.ceil() to round upwards. Now the value of the financial airbag is displayed correctly.

--- Lab2/Task_2.py
+++ Lab2/Task_2.py
@@ -7,11 +7,11 @@ increase = 0.03  # Ежемесячный рост цен
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 
-money_capital = round(
+money_capital = math.ceil(
     math.fsum(
         max(0, spend * math.pow(1 + increase, month) - salary)
         for month in range(months)
     )
 )
 
-print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", int(money_capital))
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
